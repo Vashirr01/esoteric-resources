cb3d93670e3e5081a20615fa2f1dde34793f4dd7
vashirr <130561897+Vashirr01@users.noreply.github.com>
2026-03-03 05:27:26 +0530

    feat: replace Keycloak init with Supabase AuthProvider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,28 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./lib/AuthContext";
 import App from "./App";
 import "./App.css";
-import keycloak from "./keycloak";
 
-keycloak
-  .init({ onLoad: "check-sso", pkceMethod: "S256", silentCheckSsoRedirectUri: window.location.origin + "/silent-check-sso.html" })
-  .then(() => {
-    createRoot(document.getElementById("root")!).render(
-      <StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </StrictMode>
-    );
-  })
-  .catch((err) => {
-    console.error("Keycloak init failed", err);
-  });
-
-// Auto-refresh token before it expires (only if authenticated)
-setInterval(() => {
-  if (keycloak.authenticated) {
-    keycloak.updateToken(30).catch(() => keycloak.logout());
-  }
-}, 30000);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
