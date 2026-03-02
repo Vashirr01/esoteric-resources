d3d05fd0b0874364cc7923c58fcf69f196cb274d
vashirr <130561897+Vashirr01@users.noreply.github.com>
2026-03-03 05:28:36 +0530

    chore: remove Keycloak frontend files

--- web/src/keycloak.ts
+++
@@ -1,9 +0,0 @@
-import Keycloak from "keycloak-js";
-
-const keycloak = new Keycloak({
-  url: "http://auth.localhost",
-  realm: "cloud-lab",
-  clientId: "cloud-lab-web",
-});
-
-export default keycloak;
